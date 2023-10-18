7aa98d38cb65923f5cb83e6edc28de0b847f087a
Thiago Vignolo Ferreira <thiagovignoloferreira@gmail.com>
2023-10-18 14:58:33 +0200

    First.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module es.ieslosmontecillos.di_t2a431_tableview {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.ieslosmontecillos.di_t2a431_tableview to javafx.fxml;
+    exports es.ieslosmontecillos.di_t2a431_tableview;
+}
